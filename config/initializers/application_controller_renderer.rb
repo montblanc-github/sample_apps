00f2331432cc208f8da0e039892e497e1ddf57e9
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-01 09:13:08 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '383ab8e8f8bd68e748e4b5946a47a28ca914a10c9add5bbafdfc2fcf40332013748db3216cb590757ed8e8c89923f3c897903e3eab5597c6892c9da9f6ff1260'
