3c7196c0bb26b0ffe0d0754440e221a2cc738966
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-22 06:00:39 +0000

    [Fix] Rspec内容修正

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-Rspec.describe List, 'モデルのテスト', model: :list do
+describe 'モデルのテスト' do
   it '有効な投稿内容の場合は保存されるか' do
     expect(FactoryBot.build(:list)).to be_valid
   end
