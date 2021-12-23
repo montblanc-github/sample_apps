a645f406082752a0463149e69f318a0d7709c5ad
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-23 07:02:23 +0000

    [Finish] 開発スキルアップ/ActiveStorage

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,18 @@
 class List < ApplicationRecord
-  attachment :image
+  has_one_attached :image
+  # attachment :image
 
   validates :title, presence: true
   validates :body, presence: true
   # validates :image, presence: true
+  validates :image_type
+
+  private
+
+  def image_type
+    if !image.blob.content_type.in?(%('image/jpeg image/png'))
+      image.purge # Rails6では、この1行は必要ありません
+      errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_064610) do
+ActiveRecord::Schema.define(version: 2021_12_23_064904) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2021_12_23_064610) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_id"
   end
 
 end
