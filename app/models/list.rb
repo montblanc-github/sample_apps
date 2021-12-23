cc7041dfc44f846e79b248220d27e8d9dfd24c32
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-23 07:14:29 +0000

    [Finish] 開発スキルアップ/ActiveStorage

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,14 +5,14 @@ class List < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   # validates :image, presence: true
-  validates :image_type
+  # validates :image_type
 
-  private
+  # private
 
-  def image_type
-    if !image.blob.content_type.in?(%('image/jpeg image/png'))
-      image.purge # Rails6では、この1行は必要ありません
-      errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
-    end
-  end
+  # def image_type
+  #   if !image.blob.content_type.in?(%('image/jpeg image/png'))
+  #     image.purge # Rails6では、この1行は必要ありません
+  #     errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
+  #   end
+  # end
 end
